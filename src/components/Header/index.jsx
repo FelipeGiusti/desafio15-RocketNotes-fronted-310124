058e3972c50adc3a4c4661e7b1dbfbfc41076efd
Felipe Giusti <felipeegiusti@gmail.com>
2024-02-01 17:28:00 -0300

    criando componentes iniciais

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,20 @@
+import { RiShutDownLine } from 'react-icons/ri'
+import { Container, Profile, Logout } from "./style";
+
+export function Header() {
+  return (
+    <Container>
+      <Profile>
+        <img src="https://github.com/felipegiusti.png" alt="foto do usuário" />
+        <div>
+          <span>Bem-vindo</span>
+          <strong>Felipe Giusti</strong>
+        </div>
+      </Profile>
+
+      <Logout>
+        <RiShutDownLine />
+      </Logout>
+    </Container>
+  )
+}
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,14 +1,33 @@
-import { Container } from './style'
+import { Container, Links, Content } from './style'
 import { Button } from '../../components/Button'
+import { Header } from '../../components/Header'
+import { Section } from '../../components/Section'
+import { Tag } from '../../components/Tag'
+import { ButtonText } from '../../components/ButtonText'
 
 export function Details() {
   return (
     <Container>
-      <h1>Hello World!</h1>
-      <p>Felipe Giusti</p>
-      <Button title="Entrar" loading />
-      <Button title="Cadastrar" loading />
-      <Button title="Voltar" loading />
+      <Header />
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota" />
+          <h1>Introdução ao React</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum aspernatur ullam libero iusto ipsa numquam sequi qui possimus, nihil eaque? In necessitatibus veniam ab hic quod vel ipsum, blanditiis sunt.</p>
+          <Section title="Links úteis">
+            <Links>
+              <li><a href="https://www.rocketseat.com.br/">https://www.rocketseat.com.br/</a></li>
+              <li><a href="https://www.rocketseat.com.br/">https://www.rocketseat.com.br/</a></li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="Express" />
+            <Tag title="Node" />
+          </Section>
+          <Button title="Voltar" />
+        </Content>
+      </main>
     </Container>
   )
 }
