f62d0bba7098fd28c8568a7124a0696e6b0e95f4
Felipe Giusti <felipeegiusti@gmail.com>
2024-02-26 16:42:18 -0300

    inclusão da tela de profile

--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,13 @@ import { Home } from './pages/Home'
 import { Details } from './pages/Details'
 import { SignIn } from './pages/SignIn'
 import { SignUp } from './pages/SignUp'
+import { Profile } from './pages/Profile'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <SignUp />
+      <Profile />
     </ThemeProvider>
   </React.StrictMode>
 )
---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,50 @@
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi'
+import { Container, Form, Avatar } from "./style";
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+
+export function Profile() {
+  return (
+    <Container>
+      <header>
+        <a href="/">
+          <FiArrowLeft />
+        </a>
+      </header>
+      <Form>
+        <Avatar>
+          <img src="https://github.com/felipegiusti.png" alt="Foto do usuário" />
+          <label htmlFor="avatar">
+            <FiCamera />
+            <input id='avatar' type='file' />
+          </label>
+        </Avatar>
+        <Input
+          placeholder="Nome"
+          type="text"
+          icon={FiUser}
+        />
+
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+        />
+
+        <Input
+          placeholder="Senha atual"
+          type="password"
+          icon={FiLock}
+        />
+
+        <Input
+          placeholder="Nova senha"
+          type="password"
+          icon={FiLock}
+        />
+
+        <Button title="Salvar" />
+      </Form>
+    </Container>
+  )
+}
