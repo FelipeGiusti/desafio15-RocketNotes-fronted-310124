38b1a1de5b4b2cbbc241b4ac8749284cc42cd9bb
Felipe Giusti <felipeegiusti@gmail.com>
2024-12-03 19:44:27 -0300

    Manipulação de dados em localstorage para login e logout

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,15 +1,16 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 import { api } from "../services/api";
-import { SignIn } from "../pages/SignIn";
 
 export const AuthContext = createContext({});
 
 function AuthProvider({children}){
     const [data, setData] = useState({});
-    async function SignIn({email, password}) {
+    async function signIn({email, password}) {
         try {
             const response = await api.post("/sessions", {email, password})
             const { user, token } = response.data
+            localStorage.setItem("@rocketnotes:user", JSON.stringify(user));
+            localStorage.setItem("@rocketnotes:token", token);
             api.defaults.headers.authorization = `Bearer ${token}`
             setData({user, token})
         } catch (error) {
@@ -23,8 +24,31 @@ function AuthProvider({children}){
         
     }
 
+    function signOut(){
+        localStorage.removeItem("@rocketnotes:token");
+        localStorage.removeItem("@rocketnotes:user");
+
+        setData({});
+    }
+
+    useEffect(() => {
+        const token = localStorage.getItem("@rocketnotes:token");
+        const user = localStorage.getItem("@rocketnotes:user");
+
+        if(token && user){
+            api.defaults.headers.authorization = `Bearer ${token}`;
+            setData({
+                token,
+                user: JSON.parse(user)
+            })
+        }
+    }, [])
+
     return (
-        <AuthContext.Provider value={{SignIn, user: data.user}}>
+        <AuthContext.Provider value={{
+            signIn,
+            signOut,
+            user: data.user}}>
             {children}
         </AuthContext.Provider>
     )
